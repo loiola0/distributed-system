aaf1b4790671913178faa2781c26707dc73067eb
Victor Gabriel <61810592+loiola0@users.noreply.github.com>
2021-12-10 21:35:37 -0300

    feature: code cleaning

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -11,17 +11,16 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        // establish a connection by providing host and port
-        // number
+        
         int port = 1234;
         try (Socket socket = new Socket("localhost", port)) {
-            // writing to server
+            
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
   
-            // reading from server
+            
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
   
-            // object of scanner class
+            
             Scanner sc = new Scanner(System.in);
             String line = null;
             int cont = 0;
@@ -32,21 +31,20 @@ public class Client {
             
             
             while (cont < numberRandomNumbers) {  
-                // reading from user
+               
                 int valor = gerador.nextInt(intervalRandomNumber);
                 line = Integer.toString(valor);
   
-                // sending the user input to server
+                
                 out.println(line);
                 out.flush();
   
-                // displaying server reply
-                //in.readLine();
+                
                 cont += 1;   
                 
                 valorTotal = Integer.parseInt(in.readLine());
             }
-            // closing the scanner object
+           
             
             sc.close();
             System.out.println("Valor final: "+ valorTotal);
